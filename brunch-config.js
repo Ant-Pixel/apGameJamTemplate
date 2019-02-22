d0803809d581f6a1b537b57fe84f409fafc35303
e-a-n <ean.director@gmail.com>
2019-02-22 03:11:19 -0800

    failed at getting brunch to build correctly, going to try again tomorrow using the phaser-brunch template as a starter

--- brunch-config.js
+++ brunch-config.js
@@ -1,14 +1,28 @@
 // See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {
+exports.files = {};
+exports.conventions = {
+    assets: "client/static/**"
+};
+
+exports.files.javascripts = {
     joinTo: {
       'vendor.js': /^(?!client)/, // Files that are not in `app` dir.
       'client.js': /^client/
     }
-  },
-  stylesheets: {joinTo: 'client.css'}
 };
 
+exports.files.stylesheets = {
+    joinTo: 'client.css'
+};
+
+exports.watcher = {
+    awaitWriteFinish: true,
+    usePolling: true
+}
+
 exports.plugins = {
-  babel: {presets: ['latest']}
+  babel: {
+      presets: ['latest'],
+      ignore: [/^node_modules/, /^vendor/]
+  }
 };
--- client/initialize.js
+++ client/initialize.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // do your setup here
-  console.log('Initialized app');
+    // do your setup here
+    console.log('Initialized app');
+
+    const config = require("config/config");
+
+    //Start the Phaser code
+    const game = window.GAME = new Phaser.Game(config.default.gameSpecs);
+
+    //Register all of the game states!!
+    const stateList = Object.keys(config.default.states);
+    stateList.forEach((state) => {
+        let src = require(state.src)(game, config);
+        game.state.add(state.name, src);
+        console.log(`${state.name} state has been added to the game!!`);
+    });
+
+    game.state.start(config.default.startingState);
+
 });
-const config = require("config/config");
-
-//Start the Phaser code
-const game = window.GAME = new Phaser.Game(config.default.gameSpecs);
-
-//Register all of the game states!!
-const stateList = Object.keys(config.default.states);
-stateList.forEach((state) => {
-    let src = require(state.src)(game, config);
-    game.state.add(state.name, src);
-    console.log(`${state.name} state has been added to the game!!`);
-});
-
-game.state.start(config.default.startingState);
